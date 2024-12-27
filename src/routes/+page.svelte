3ae0d7b1c63464f59419f8dc381b4f1f0a53fe0d
dagmawibabi <1babidagi@gmail.com>
2024-12-27 16:43:20 +0300

    starter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import logo from '$lib/assets/logo.jpg';
+</script>
+
+<div class="flex h-screen w-screen flex-col items-center justify-center">
+	<img src={logo} alt="logo" class="w-32" />
+
+	<a href="/auth" class="p-2 hover:bg-black hover:text-white"> AUTH PAGE</a>
+	<a href="/homepage" class="p-2 hover:bg-black hover:text-white"> HOME PAGE</a>
+</div>
---
+++ src/routes/homepage/+page.svelte
@@ -0,0 +1,4 @@
+<div class="flex h-screen w-screen flex-col items-center justify-center">
+	<span class="pb-4">THIS IS HOMEPAGE</span>
+	<a href="/" class="p-2 hover:bg-black hover:text-white"> GO BACK </a>
+</div>
