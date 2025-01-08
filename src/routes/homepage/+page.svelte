f4de119562e3ba78207f9f6ee674ea7d9729c484
freab <fremesfin1@gmail.com>
2025-01-08 19:58:50 +0300

    custum font and better deisgn on the footer

--- src/routes/homepage/+page.svelte
+++ src/routes/homepage/+page.svelte
@@ -3,13 +3,17 @@
 	import Nav from '../../components/Nav.svelte';
 	import Scene from '../../components/Scene.svelte';
 	import Featured from '../../components/Featured.svelte';
+	import Footer from '../../components/Footer.svelte';
+	import AboutUs from '../../components/AboutUs.svelte';
 </script>
 
 <Nav />
 <Scene />
 <Featured />
+<AboutUs />
 
 <div class="flex h-screen w-screen flex-col items-center justify-center">
 	<span class="pb-4">THIS IS HOMEPAGE</span>
 	<a href="/" class="p-2 hover:bg-black hover:text-white"> GO BACK </a>
 </div>
+<Footer />
